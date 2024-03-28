f8d5afe9683afe8ff5efdc3316d6898b93a8cfdb
Robin Jünemann <robin@juenemanns.de>
2024-03-28 15:22:24 +0100

    updated page design

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,13 @@
       <button @click="checkInput" id="check-btn">Check</button>
       <div id="result">{{ result }}</div>
     </div>
+    <div class="box">
+      <p>A word, phrase, or sequence that reads the same backwards as forwards, e.g. madam or nurses run.
+</p>
+    </div>
+    <div class="rowContent">
+
+    </div>
   </div>
 
 </template>
@@ -38,35 +45,66 @@ export default {
 </script>
 
 <style scoped>
+body {
+  height: 100%;
+
+}
+h1 {
+  color: black;
+  padding: 2rem;
+  text-align: center;
+}
+p {
+  color: #FAFAF5;
+  margin-top: 2rem;
+}
+#result {
+  color: #FAFAF5;
+}
 .container {
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  flex-wrap: wrap;
-  align-content: center;
+
+  background: #e8ebeb;
+  min-width: 30rem;
+  width: 75%;
+  height: 100vh;
 }
 .box {
   height: auto;
   width: 20rem;
+  padding: 2rem;
+  margin: 2rem;
   display: flex;
   justify-content: center;
   flex-direction: column;
+  background-color: #9D975C;
   margin: auto;
+  border-radius: 2px;
+  flex: 0 1 auto;
+}
+.rowContainer {
+  flex: 1 1 auto;
+
 }
 button {
   margin: 1rem;
   width: fit-content;
   align-self: center;
+  border-radius: 5px;
+  padding: 0.2rem 1rem;
+  background-color: #4F4A7D;
+  color: #FAFAF5;
+  border-color: #4F4A7D;
+  border: none;
+  
+}
+
+button:hover {
+  box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24),0 17px 50px 0 rgba(0,0,0,0.19);
+
 }
 
 #result {
   align-self: center;
 }
-@media (min-width: 1024px) {
 
-}
-@media (prefers-color-scheme: dark) {
-
-} 
-/* Add your scoped styles here if needed */
 </style>
